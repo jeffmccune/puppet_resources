bdcdfdbbeee50e835b93a12943115e40c2220d21
Jeff McCune <jeff@openinfrastructure.co>
2017-06-14 15:03:05 -0400

    Syntax check only files changed

--- control-repo/scripts/syntax_check.sh
+++ control-repo/scripts/syntax_check.sh
@@ -16,33 +16,58 @@ fi
 # Use Puppet Enterprise Ruby to check ruby and yaml files
 export PATH="/opt/puppetlabs/puppet/bin:$PATH"
 
+# List the files changes from $BASEBRANCH on stdout
+files_changed() {
+  # File status flags:
+  # M modified - File has been modified
+  # C copy-edit - File has been copied and modified
+  # R rename-edit - File has been renamed and modified
+  # A added - File has been added
+  # D deleted - File has been deleted
+  # U unmerged - File has conflicts after a merge
+  git diff --name-status "${BASEBRANCH:=production}" \
+    | awk '$1 ~ /^[MCRA]$/' \
+    | cut -f2-
+}
+
 # Check the Puppetfile
 echo -n "Checking Puppetfile ... "
 ruby -c Puppetfile
 
-# Find all the script and run syntax check
-find . -name '.git' -o -name '*.sh' -print0 \
-  | xargs --no-run-if-empty -0 -t -P$cores -n1 \
+files_changed \
+  | awk '/\.(sh)$/' \
+  | xargs --no-run-if-empty -t -P$cores -n1 \
   bash -n
 
 # Check all YAML files
 # See: http://stackoverflow.com/questions/3971822/yaml-syntax-validator
-find . -name '.git' -o -name '*.yml' -print0 \
-  | xargs --no-run-if-empty -0 -t -P$cores -n1 \
-  ruby -r yaml -e 'YAML.load_file(ARGV[0])'
-
-find . -name '.git' -o -name '*.yaml' -print0 \
-  | xargs --no-run-if-empty -0 -t -P$cores -n1 \
+files_changed \
+  | awk '/\.(yml|yaml)$/' \
+  | xargs --no-run-if-empty -t -P$cores -n1 \
   ruby -r yaml -e 'YAML.load_file(ARGV[0])'
 
 # Check all JSON files
-find . -name '.git' -o -name '*.json' -print0 \
-  | xargs --no-run-if-empty -0 -t -P$cores -n1 \
+files_changed \
+  | awk '/\.(json)$/' \
+  | xargs --no-run-if-empty -t -P$cores -n1 \
   ruby -r json -e 'JSON.load(File.read(ARGV[0]))'
 
-find . -name '.git' -o -name '*.rb' -print0 \
-  | xargs --no-run-if-empty -0 -t -P$cores -n1 \
+files_changed \
+  | awk '/\.(rb)$/' \
+  | xargs --no-run-if-empty -t -P$cores -n1 \
   ruby -c
+
+# Check all erb files
+files_changed \
+  | awk '/\.(erb)$/' \
+  | xargs -l --no-run-if-empty -t -P$cores -n1 \
+  bash -c 'erb -P -x -T- $0 | ruby -c'
+
+# Check all Puppet manifest files
+files_changed \
+  | awk '/manifests\/.*\.(pp)$/' \
+  | xargs --no-run-if-empty -t -P$cores -n1 \
+  puppet parser validate
 
 # vim:tabstop=2
 # vim:shiftwidth=2
